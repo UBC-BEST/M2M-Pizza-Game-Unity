20c44f3f01c5d61ccc7c28044d66486c9a909875
Kennedy Yung <97806971+kennedyyung@users.noreply.github.com>
2023-12-02 11:47:10 -0800

    changing starting positions

--- Assets/Scripts/GameScene/ToppingBins.cs
+++ Assets/Scripts/GameScene/ToppingBins.cs
@@ -1,14 +1,3 @@
-
-// using UnityEngine;
-
-// public class ToppingBin : MonoBehaviour
-// {
-//     public GameObject toppingPrefab; // Reference to the prefab you want to instantiate
-//     public int numberOfToppings = 4; // Number of toppings to instantiate
-//     public float distanceBetweenToppings = 2f; // Distance between each instantiated topping
-//     public float distanceFromScreenSides = 2f; // Distance from the sides of the screen
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,10 +8,12 @@ public class ToppingBinsScript : MonoBehaviour
     [SerializeField] Rigidbody2D oliveBin;
     [SerializeField] Rigidbody2D greenPepperBin;
     [SerializeField] Rigidbody2D sausageBin;
+
     [SerializeField] Vector2 startingPositionO = new Vector2(-3.3f, 6.5f);
     [SerializeField] Vector2 startingPositionP = new Vector2(-0.5f, 6.5f);
     [SerializeField] Vector2 startingPositionS = new Vector2(2.3f, 6.5f);
     [SerializeField] Vector2 startingPositionG = new Vector2(5.1f, 6.5f);
+
     [SerializeField] Vector2 endPositionO = new Vector2(-3.3f, 3.4f);
     [SerializeField] Vector2 endPositionP = new Vector2(-0.5f, 3.4f); 
     [SerializeField] Vector2 endPositionS = new Vector2(2.3f, 3.4f); 
@@ -31,6 +22,7 @@ public class ToppingBinsScript : MonoBehaviour
 
         void Start()
     {
+
         StartCoroutine(MoveBin(oliveBin, startingPositionO, endPositionO));
         StartCoroutine(MoveBin(pepperoniBin, startingPositionP, endPositionP));
         StartCoroutine(MoveBin(sausageBin, startingPositionS, endPositionS));
@@ -42,7 +34,7 @@ public class ToppingBinsScript : MonoBehaviour
         rigidbody.MovePosition(start);
 
         float startTime = Time.time;
-        float duration = 1.0f; // Adjust the duration as needed
+        float duration = 1.0f; 
 
         while (Time.time - startTime < duration)
         {
@@ -55,26 +47,4 @@ public class ToppingBinsScript : MonoBehaviour
     }
 
 
-    //     void Start()
-    // {
-    //     SetInitialPosition(oliveBin, startingPositionO);
-    //     SetInitialPosition(pepperoniBin, startingPositionP);
-    //     SetInitialPosition(sausageBin, startingPositionS);
-    //     SetInitialPosition(greenPepperBin, startingPositionG);
-    // }
-
-    // void SetInitialPosition(Rigidbody2D rigidbody, Vector2 position)
-    // {
-    //     rigidbody.MovePosition(position);
-    // }
-
-
-
-    // void Start()
-    // {
-    //     pepperoniBin.transform.position = startingPositionP;
-    //     oliveBin.transform.position = startingPositionO;
-    //     greenPepperBin.transform.position = startingPositionG;
-    //     sausageBin.transform.position = startingPositionS;
-    // }
 }
